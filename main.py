03b85a7de6a04ab7777499791829b3a9fbb5e2bd
Tatiana <45382631+Tatfi32@users.noreply.github.com>
2020-10-16 19:35:19 +0300

    Visual part for different sequences

--- main.py
+++ main.py
@@ -1,65 +1,76 @@
 # ! in package.PcapParser.seq_processing() front hemisphere is selected only. Beware!
 import package
 import pandas as pd
-import pickle
 # Do not remove this line its required by scatter3D indirectly
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 from pathlib import Path
 
 """
-get list of sequences path to be loop thought in one series (one relief) group
-each path processed with constant scripts:
+1- get list of sequences path to be loop thought in one series (one relief) group
+2 - each path processed with constant scripts:
                                             video_parser(path to the video folder)
                                             read_pcap(path to the pcap folder)
                                             camera(path to the frames folder)
                         dynamic scripts: 
                                             projection(camera_position =  [bias value, 0 ,0] position)
-                                            
-project all points_cloud from  projection script to one image                                     
-                    visualizator.plot_picture
-               
-"""
-sequence_path = Path.cwd() / 'data'
-result = pd.DataFrame()
-sequence_paths = [x for x in sequence_path.iterdir() if x.is_dir()]
 
+3 - project all points_cloud from  projection script to one image                                     
+4 - save one created image with visualizator.plot_picture
+
+"""
+# "data" folder contain from 6 to 8 sub-folders with data from one static flat relief
+# each sub_folder contain information about one movement across vertical camera axes (sequence)
+# each sub_folder contain *pcap, *avi and calib data files
+sequence_path = Path.cwd() / '1'
+sequence_paths = [x for x in sequence_path.iterdir() if x.is_dir()]
 camera_seq_position = [-0.75, 0, 0]
 
-for seq_path in sequence_paths:
-    #Video Parsing
+# create empty DataFrame to accumulate lidar points sequences from each sub_folder
+result = pd.DataFrame()
+
+for index, seq_path in enumerate(sequence_paths):
+    # Video Parsing
     parser = package.Parser(path=seq_path)
     parser.read()
 
     # Reading pcap binaries
+    # take just first *pcap file because all *pcap folder files from the same relief
     pcap_file_path = [x for x in seq_path.glob('*.pcap*') if x.is_file()][0]
     pcap = package.PcapReader(pcap_file_path)
     lidar_data = pcap.get_pcap_data()
 
+    # Plot points cloud for one sequence before projection
+    visualizator = package.Visualizator(output_path=seq_path, df=lidar_data)
+    visualizator.plot_scatter_one_seq()
+
     # Projecting on camera
 
     # Selecting pixels corresponding to the board
-    constraints = {'x_min': -708, 'x_max': 292, 'y_min': -410, 'y_max': 190 }
+    constraints = {'x_min': -708, 'x_max': 292, 'y_min': -410, 'y_max': 190}
     camera = package.Camera(constraints=constraints)
 
-    projector = package.Projector(camera_position = camera_seq_position)
-    camera_seq_position[0] = camera_seq_position[0] + 0.009
+    projector = package.Projector(camera_position=camera_seq_position)
+    camera_seq_position[0] = camera_seq_position[0] + 0.05
 
     data = projector.project(lidar_data)
     data = camera.project(data)
 
     # Additional depth constraint to remove falling out points
     data = data[data['Z'] < 1.3]
-    data = data[data['Z'] > 1 ]
-    print(result.shape)
+    data = data[data['Z'] > 1]
+    data['seq_number'] = index
+
     result = pd.concat([result, data], ignore_index=True, sort=False)
 
-# Visualization without picture (point cloud)
-visualizator = package.Visualizator(df=result)
-visualizator.plot_points(output_path=seq_path)
+# Plot point cloud lidar series from several lidar position on one figure (without picture)
+# Different color represent diferent lidar position in each sequence
+visualizator = package.Visualizator(sequence_path, df=result)
+visualizator.plot_seq_points(df=result)
 
-# Visualization with picture (!!!all point goes to the same frame,because camera position was static)
-images_file_path = [x for x in (seq_path/'frames').glob('*.bmp*') if x.is_file()][0]
-visualizator.plot_picture(str(images_file_path), output_path=seq_path)
+# Visualization with picture
+# !!!all point goes to the same frame,because camera position was static
+images_file_path = [x for x in (seq_path / 'frames').glob('*.bmp*') if x.is_file()][0]
+visualizator.plot_picture(str(images_file_path))
 
 
 
--- package/visualizator.py
+++ package/visualizator.py
@@ -1,46 +1,110 @@
 import matplotlib.pyplot as plt
 from PIL import Image, ImageOps
 import pickle
+import numpy as np
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+from scipy import misc
 
 __all__ = ['Visualizator']
 
+
 class Visualizator:
 
-    def __init__(self, df):
+    def __init__(self, output_path, df):
         self.df = df
+        self.output_path = output_path
 
-    def plot_points(self,output_path):
+    # plot 3D scatter based on certain  X,Y,Z,Depth values
+
+    def plot_scatter_one_seq(self, ):
+        fig = plt.figure()
+        """
+        ax_1 = fig.add_subplot(131, projection='3d')
+        pnt3d_1 = ax_1.scatter(x, y, z, c=depth, cmap=plt.cm.BuPu_r)
+        ax_1.set_xlabel('x Label')
+        ax_1.set_ylabel('y Label')
+        ax_1.set_zlabel('Z Label')
+        cbar_1 = plt.colorbar(pnt3d_1)
+
+        ax_3 = fig.add_subplot(133, projection='3d')
+        pnt3d_3 = ax_3.scatter(z, x, y, c=depth, cmap=plt.cm.BuPu_r)
+        cbar_3 = plt.colorbar(pnt3d_3)
+        ax_3.set_xlabel('Z Label')
+        ax_3.set_ylabel('x Label')
+        ax_3.set_zlabel('y Label')
+        """
+
+        ax_2 = fig.add_subplot(111, projection='3d')
+
+        pnt3d_2 = ax_2.scatter(self.df.Y, self.df.Z, self.df.X, c=self.df.D, cmap=plt.cm.BuPu_r)
+        cbar_2 = plt.colorbar(pnt3d_2)
+        ax_2.set_xlabel('y Label')
+        ax_2.set_ylabel('Z Label')
+        ax_2.set_zlabel('x Label')
+
+        path = str(self.output_path / 'sequence_3D_points.png')
+        fig.savefig(path, dpi=100)
+        #plt.show()
+
+    def plot_seq_points(self, df):
         fig = plt.figure()
         ax = plt.axes(projection='3d')
-        ax.scatter3D(self.df.u, self.df.v, self.df.Z, c=self.df.Z, cmap='Greens')
 
-        ax.set_title('Surface plot')
-        ax.set_xlabel('u Label')
-        ax.set_ylabel('v Label')
+        categories = np.unique(df['seq_number'])
+        colors = np.linspace(0, 1, len(categories))
+        colordict = dict(zip(categories, colors))
+
+        df["Color"] = df['seq_number'].apply(lambda x: colordict[x])
+        sc = ax.scatter3D(df.v, df.u, df.Z, c=df.Color)
+        colorize = plt.colorbar(sc, orientation="horizontal")
+        colorize.set_label("Sequence number")
+
+        ax.set_title('Sequence surface plot')
+        ax.set_xlabel('v Label')
+        ax.set_ylabel('u Label')
         ax.set_zlabel('Z Label')
 
-        path = str(output_path / 'points.png')
+        path = str(self.output_path / 'sequence_points.png')
         fig.savefig(path, dpi=100)
         plt.show()
 
-
-
-    def plot_picture(self, image_path, output_path):
+    def plot_picture(self, image_path):
         fig, ax = plt.subplots(figsize=(20, 10), dpi=60)
         img = Image.open(image_path)
-        left = 252
-        top = 350
-        right = 1252
-        bottom = 950
+        width, height = img.size
 
-        im = img.crop((left, top, right, bottom))
+        left = width / 2 + self.df.u.min()  # 252
+        upper = height / 2 - self.df.v.max()  # 350
+        right = width / 2 + self.df.u.max()  # 1252
+        lower = height / 2 - self.df.v.min()  # 950
+
+        im = img.crop((left, upper, right, lower))
         plt.imshow(im, extent=[self.df.u.min(), self.df.u.max(), self.df.v.min(), self.df.v.max()], zorder=0,
                    aspect='auto')
         sc = plt.scatter(self.df.u, self.df.v, c=self.df.Z, zorder=1, s=10)
         colorize = plt.colorbar(sc, orientation="horizontal")
         colorize.set_label("Distance (m)")
 
-        path = str(output_path / 'points_on_pic.png')
+        path = str(self.output_path / 'points_on_image.png')
         fig.savefig(path)
-
         plt.show()
+
+    # not used class functions
+    """
+    #Plot 3D scatter from Visualizator.class dataframe       
+    def plot_points(self, ):
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.scatter3D(self.df.v, self.df.u, self.df.Z, cmap="Purples")
+
+        ax.set_title('Surface plot')
+        ax.set_xlabel('v Label')
+        ax.set_ylabel('u Label')
+        ax.set_zlabel('Z Label')
+
+        path = str(self.output_path / 'points.png')
+        fig.savefig(path, dpi=100)
+        plt.show()
+
+    """
